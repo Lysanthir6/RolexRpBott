cfb1315f0851780ead6fc8fb8590984a0d039f05
Lysanthir6 <69094854+Lysanthir6@users.noreply.github.com>
2020-08-02 03:22:21 +0300

    Add files via upload

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,5 @@
+module.exports = member => {
+    let username = member.user.username;
+    member.sendMessage('https://media.giphy.com/media/14fX2WQuOIVHBC/giphy.gif Selamlar,Değerli **' + username + '** RolexRp Sunucusuna Hoşgeldin 🔥 #kurallar a bakmayı unutma,lazım olacak. :thinking: Keyifli Vakit Geçirmen Dileğiyle http://gph.is/1c4s4EC :wink:');
+
+};
